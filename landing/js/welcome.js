1172ac281936bee42e83df490c0f478392470315
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-05-01 19:00:12 +0200

    no message

--- landing/js/welcome.js
+++ landing/js/welcome.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     $(".main").onepage_scroll({
-     sectionContainer: "section",    
-     easing: "ease",                     
-     animationTime: 1000,             
-     pagination: true,                
-     updateURL: true,                
-     beforeMove: function(index) {},  
-     afterMove: function(index) {},  
-     loop: false,                   
-     keyboard: true,                
-     responsiveFallback: false       
- });
+       sectionContainer: "section",    
+       easing: "ease",                     
+       animationTime: 1000,             
+       pagination: true,                
+       updateURL: true,                
+       beforeMove: function(index) {},  
+       afterMove: function(index) {},  
+       loop: false,                   
+       keyboard: true,                
+       responsiveFallback: false       
+   });
     $('.go-to').click(function(){
         var target = $(this).attr('go-to');
         $('.main').moveTo(target);
